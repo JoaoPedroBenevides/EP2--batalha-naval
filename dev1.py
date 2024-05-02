26e4d14e4825f6b3e2764e7ae75efacb42be346f
Joao Pedro Benevides <joaopbp2@al.insper.edu.br>
2024-05-01 20:19:50 -0400

    segunda e primeira funcao

--- dev1.py
+++ dev1.py
@@ -10,3 +10,25 @@ def cria_mapa(n):
         mapa.append(colunas)
 
     return mapa
+
+
+def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
+    tamanho_mapa = len(mapa)   
+
+    if linha < 0 or coluna < 0 or linha >= tamanho_mapa or coluna >= tamanho_mapa:
+        return False
+    
+    if orientacao == 'v':
+        for i in range(blocos):
+            if linha + i >= tamanho_mapa or mapa[linha + i][coluna] != ' ':
+                return False
+
+    elif orientacao == 'h':
+        for i in range(blocos):
+            if coluna + i >= tamanho_mapa or mapa[linha][coluna + i] != ' ':
+                return False
+                
+    else:
+        return False
+    
+    return True
